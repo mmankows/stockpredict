4def5c81f494e6272f8112ff1b74d760f699d2d8
Michał Mankowski <michal.mankowski@manganium.com>
2017-07-20 19:56:35 +0200

    basic csv parsing and initial cli

---
+++ scli.py
@@ -0,0 +1,42 @@
+import cli.app
+import logging
+
+from stockpredict.training import NetworkTrainer
+from stockpredict.network import StockNeuralNetwork
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+
+
+@cli.app.CommandLineApp
+def train(app):
+    """
+    Trains a network with a CSV file data
+    :param app: 
+    :return: 
+    """
+    # Choose Trainer
+    trainer = NetworkTrainer()
+
+    # Choose network for or create new one
+    logger.info("Setting up network.")
+    net = StockNeuralNetwork(previous=365, future=10)
+    trainer.set_network(net)
+
+    # Run training
+    logger.info("Starting training from {} file.".format(app.params.input_csv))
+    trainer.train_from_csv(csv_path=app.params.input_csv, col_dict={
+        'DATE': 'Data',
+        'OPEN': 'Otwarcie',
+        'CLOSE': 'Zamkniecie',
+        'LOW': 'Najnizszy',
+        'HIGH': 'Najwyzszy',
+        'VOLUME': 'Wolumen'
+    })
+    # Data,Otwarcie,Najwyzszy,Najnizszy,Zamkniecie,Wolumen
+
+
+train.add_param("-i", "--input-csv", help="Input csv file with stock data", required=True, action="store")
+
+if __name__ == "__main__":
+    train.run()
--- stockpredict/training.py
+++ stockpredict/training.py
@@ -1,22 +1,51 @@
-from csv import DictReader
+from csv import reader, DictReader
+import logging
 
 from stockpredict.network import SessionData
 
 
+logger = logging.getLogger()
+
+
 class NetworkTrainer(object):
+    """
+    Trains neural network with data
+    """
 
     def __init__(self):
-        pass
+        self._network = None
 
-    def load_session_data(self, csv_path, col_dict):
-        with open(csv_path, 'r') as csv_file:
-            reader = DictReader(csv_path)
+    def set_network(self, network):
+        self._network = network
+
+    def train_from_csv(self, csv_path, col_dict=None):
+        col_dict = col_dict or {}
+        print csv_path
+        data_cnt = 0
+        error_cnt = 0
+        with open(csv_path, 'rb') as csv_file:
+            reader = DictReader(csv_file)
             for row in reader:
-                yield SessionData(
-                    date=row[col_dict['DATE']],
-                    price_open=row[col_dict['PRICE_OPEN']],
-                    price_close=row[col_dict['PRICE_CLOSE']],
-                    price_low=row[col_dict['PRICE_LOW']],
-                    price_high=row[col_dict['PRICE_HIGH']],
-                    volume=row[col_dict['VOLUME']]
-                )
+
+                try:
+                    SessionData(
+                        date=row[col_dict['DATE']],
+                        price_open=row[col_dict['OPEN']],
+                        price_close=row[col_dict['CLOSE']],
+                        price_low=row[col_dict['LOW']],
+                        price_high=row[col_dict['HIGH']],
+                        volume=row[col_dict['VOLUME']]
+                    )
+#                    logger.debug("Row parsed: {}".format(row))
+                    data_cnt += 1
+
+                except KeyError as e:
+                    raise KeyError("CSV header doesn't match provided column mapping!: {}".format(e))
+
+                except TypeError:
+                    logger.error("SKIPPED RECORD! - bad data type")
+                    error_cnt += 1
+
+        logger.info("Parsed {} data rows".format(data_cnt))
+        if error_cnt:
+            logger.warn("{} rows skipped due to errors".format(error_cnt))
